9183848aa343c9454e98b581d6f3db8ab794a5f2
ykt68 <ykt68@macbook15.local>
2017-10-29 01:07:02 +0900

    コメントを it の引数にする。

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,16 +1,10 @@
 require 'rails_helper'
 
 describe Contact do
-  # 姓と名とメールがあれば有効な状態であること
-  it 'is valid with a firstname, lastname and email'
-  # 名がなければ無効な状態であること
-  it 'is invalid without a firstname'
-  # 姓がなければ無効な状態であること
-  it 'is invalid without a lastname'
-  # メールアドレスがなければ無効な状態であること
-  it 'is invalid without an email address'
-  # 重複したメールアドレスなら無効な状態であること
-  it 'is invalid with a duplicate email address'
-  # 連絡先のフルネームを文字列として返すこと
-  it 'returns a contact\'s full name as a string'
+  it '姓と名とメールがあれば有効な状態であること'
+  it '名がなければ無効な状態であること'
+  it '姓がなければ無効な状態であること'
+  it 'メールアドレスがなければ無効な状態であること'
+  it '重複したメールアドレスなら無効な状態であること'
+  it '連絡先のフルネームを文字列として返すこと'
 end
