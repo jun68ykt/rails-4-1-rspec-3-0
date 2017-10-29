5d58822cdf36d9c4db38af5ef40bd64bf48ce06a
ykt68 <ykt68@macbook15.local>
2017-10-30 04:53:44 +0900

    Phoneの継承ファクトリを使うように修正

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -1,17 +1,21 @@
 require 'rails_helper'
 
 describe Phone do
+
+  it 'ファクトリが有効なオブジェクトを返すこと' do
+    phone = build :phone
+    expect(phone).to be_valid
+  end
+
   it '連絡先ごとに重複した電話番号を許可しないこと' do
-    contact = Contact.create(
-        firstname: 'Joe',
-        lastname: 'Tester',
-        email: 'joetester@example.com'
-    )
-    contact.phones.create(
-        phone_type: 'home',
+    contact = create :contact
+
+    create(:home_phone,
+        contact: contact,
         phone: '785-555-1234'
     )
-    mobile_phone = contact.phones.build(
+    mobile_phone = build(:mobile_phone,
+        contact: contact,
         phone_type: 'mobile',
         phone: '785-555-1234'
     )
@@ -21,18 +25,14 @@ describe Phone do
   end
 
   it '2 件の連絡先で同じ電話番号を共有できること' do
-    contact = Contact.create(
-        firstname: 'Joe',
-        lastname: 'Tester',
-        email: 'joetester@example.com'
-    )
-    contact.phones.create(
-        phone_type: 'home',
+
+    create(
+        :home_phone,
         phone: '785-555-1234'
     )
-    other_contact = Contact.new
-    other_phone = other_contact.phones.build(
-        phone_type: 'home',
+
+    other_phone = build(
+        :home_phone,
         phone: '785-555-1234'
     )
 
